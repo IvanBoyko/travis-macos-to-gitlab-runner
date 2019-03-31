0f8f5f20645c3580997bb9c1660bcca614c57676
Ivan Boyko <boiko.ivan@gmail.com>
2019-03-31 19:04:50 +0200

    run.sh - now register and run gitlab-runner

--- run.sh
+++ run.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 
+
+#
+# Parameters default values
+#
+
+GITLAB_URL=${GITLAB_URL:-https://gitlab.com/}
+RUNNER_NAME=${RUNNER_NAME:-$(hostname)}
+GITLAB_RUNNER_TAG_LIST=${GITLAB_RUNNER_TAG_LIST:-macos}
+
+
+#
+# Main
+#
 
 brew update
 brew upgrade carthage
 gem install fastlane
+
+
+sudo curl --output /usr/local/bin/gitlab-runner \
+    https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-darwin-amd64
+
+sudo chmod +x /usr/local/bin/gitlab-runner
+
+gitlab-runner install
+
+gitlab-runner register --non-interactive \
+    --url $GITLAB_URL \
+    --registration-token $GITLAB_REGISTRATION_TOKEN \
+    --tag-list $GITLAB_RUNNER_TAG_LIST \
+    --name $RUNNER_NAME \
+    --executor shell
+
+gitlab-runner run
