d6040fc888fc1531d31bcfff7123d23323fd4644
Ivan Boyko <boiko.ivan@gmail.com>
2019-03-31 19:08:36 +0200

    run.sh - split to install_prereqs.sh and run_gitlab_runner.sh

---
+++ install_prereqs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux
+
+
+brew update
+brew upgrade carthage
+gem install fastlane
--- run_gitlab_runner.sh
+++ run_gitlab_runner.sh
@@ -16,11 +16,6 @@ GITLAB_RUNNER_TAG_LIST=${GITLAB_RUNNER_TAG_LIST:-macos}
 # Main
 #
 
-brew update
-brew upgrade carthage
-gem install fastlane
-
-
 sudo curl --output /usr/local/bin/gitlab-runner \
     https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-darwin-amd64
 
